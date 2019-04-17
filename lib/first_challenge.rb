cf54bfb53cb1fd6d427552d815a9c3e6447a5cce
Conrad Casper <conradcasper@icloud.com>
2019-04-17 18:52:37 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -14,9 +14,18 @@ def first_challenge
   }
 
   #your code here
+  contacts["Freddy Mercury"].each do |k, v|
+    if k == :favorite_icecream_flavors 
+      v.delete_if {|x| x == "strawberry"}
+      
+    
 
-
+end
+end
   #remember to return your newly altered contacts hash!
   contacts
+
+
 end
+
 
